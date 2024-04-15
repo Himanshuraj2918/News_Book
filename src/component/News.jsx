5f7b4925a39a57f34d0df43e6561a0bc60f409bb
Himanshuraj Tiwari <98225176+Himanshuraj2918@users.noreply.github.com>
2024-04-15 22:54:46 +0530

    Update News Component

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -1,48 +1,63 @@
 import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
+import Shimmer from "./Shimmer";
 
 const News = (props) => {
+  const API_URL = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.type}&apiKey=${props.apikey}`
+       
   const [newsData, setnewsData] = useState([])
- 
-  useEffect(() => {
-    <div>Data Fetching..................</div>
-    fetchData();
-  }, [])
+  const [fetchError,setFetchError] = useState()
+  const [isLoading,setIsLoading] = useState(true)
 
-   
-const fetchData = async () =>{
-  const response = await fetch(
-    
-    `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.type}&apiKey=8cff025a62d745138caf318070c62e5c`
-  );
-  const data = await response.json();
-  console.log(data);
-  setnewsData(data.articles)
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        console.log(data);
+        setnewsData(data.articles)
+      }
+      catch (error) {
+        setFetchError(error.message)
+      }finally{
+        setIsLoading(false)
+      }
+    }
+    setTimeout(() => {
+      fetchData()
+    }, 3000);
+  }, [props])
+
+
+
 
 
 
   return (
     <>
       <div className="mx-5 mt-5">
-
-        <div className="flex">
+       
+       {isLoading &&<div className="flex flex-wrap justify-center">
+        
+        <Shimmer/> <Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/><Shimmer/></div>}
+       {fetchError && <p className="text-center text-red-600 text-3xl font-semibold">{`${fetchError}`} !!</p> }
+      {!fetchError &&  <div className="flex">
           <div className="flex flex-wrap  ">
             {
-              newsData.map((news) => <NewsItem  news={news} />)
+              newsData.map((news, i) => <NewsItem key={i} news={news} />)
             }
 
           </div>
 
 
-        </div>
+        </div>}
       </div>
     </>
   );
 };
 
 News.defaultProps = {
-  country : "in"
+  country: "in"
 }
 
 export default News;
